daf035591898ad25c83597ba3449b46c5572094a
Mohit Dewani <mohitcap177@gmail.com>
2025-08-31 02:41:55 +0530

    Changed the database url to PostgreSQL

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cryptnest.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("SQLALCHEMY_DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
