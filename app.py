b50554581cace27446602bde6db504548c74d947
Mohit Dewani <mohitcap177@gmail.com>
2025-08-31 16:57:51 +0530

    Forgot to chnage the database uri to get from env

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ from scipy.spatial.distance import euclidean # type: ignore
 load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # Supabase URL
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 db = SQLAlchemy(app)
